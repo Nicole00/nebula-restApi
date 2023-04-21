f3dedbc0c4fceb0b11afa2abd4e5dc4d5405683b
Anqi <anqi.wang@vesoft.com>
2023-04-21 17:25:22 +0800

    kill old server

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
  mvn clean package -Dmaven.test.skip=true
  cp target/nebula-restApi-1.0-SNAPSHOT.jar .
+ ps -wf | grep "nebula-restApi-1.0-SNAPSHOT.jar" | grep -v grep | awk '{print $2}'|xargs kill -9
  nohup java -jar nebula-restApi-1.0-SNAPSHOT.jar &
